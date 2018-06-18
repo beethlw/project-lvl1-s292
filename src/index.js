8edd315cb40bd230c44c50e19230c54be85e3f2d
beethlw <beeth@mail.ru>
2018-06-18 21:00:20 +0300

    add Ask the name

---
+++ src/index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+console.log('Welcome to the Brain Games!\n');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`); 
