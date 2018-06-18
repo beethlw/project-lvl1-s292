3ede8eed766c4f4a87e98143de73bd2ddf2b4b27
beethlw <beeth@mail.ru>
2018-06-18 22:19:13 +0300

    improve ask the name

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-#!/usr/bin/env node
 import readlineSync from 'readline-sync';
-console.log('Welcome to the Brain Games!\n');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`); 
+export const askName = () => readlineSync.question('May I have your name? ');
+
+export default askName;
